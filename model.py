5c56d749f55f7c7a7312d4eb2349596b0f82b0f6
henriqueboneto <h.boneto@outlook.com>
2022-01-21 14:44:11 -0300

    domain

---
+++ model.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional, Set
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self._purchased_quantity = qty
+        self.eta = eta
+        self._allocations: Set[OrderLine] = set()
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
