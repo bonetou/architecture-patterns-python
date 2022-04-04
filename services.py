505151e409913d0c6fffdf3d7be5e9e34b3d6970
bonetou <h.boneto@outlook.com>
2022-04-04 01:56:07 -0300

    chapter 4: service layer/use case

---
+++ services.py
@@ -0,0 +1,21 @@
+import model
+import repository
+from model import OrderLine
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
+def allocate(line: OrderLine, repo: repository.AbstractRepository, session) -> str:
+    batches = repo.list()
+    if is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid Sku {line.sku}")
+
+    batchref = model.allocate(line, batches)
+    session.commit()
+    return batchref
